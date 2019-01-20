ecbe47e631a0aad97fb17b79dcc7ee76e279663b
arturbaboskin <Arty212@mail.ru>
2019-01-20 20:09:11 +0300

    load exercise

--- app/src/main/java/com/example/fitnes/MainActivity.java
+++ app/src/main/java/com/example/fitnes/MainActivity.java
@@ -1,46 +1,37 @@
 package com.example.fitnes;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import APIParse.Exercise;
-import APIParse.ExerciseList;
 import APIParse.IMainView;
 import APIParse.MainPresenter;
-import retrofit2.Call;
-
 
 public class MainActivity extends MvpAppCompatActivity implements IMainView {
 
     @InjectPresenter
     public MainPresenter presenter;
-
     private Button btn;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(getApplicationContext(), TrainingChoosing.class);
-        startActivity(intent);
-
-
-        //presenter.info();
+        /*Intent intent = new Intent(getApplicationContext(), TrainingChoosing.class);
+        startActivity(intent);*/
+        presenter.info();
     }
 
 
     @Override
-    public void getExercise(ArrayList<Exercise> exercises) {
-
+    public void getExercise(List<Exercise> exercises) {
+        Log.d("MyLog", exercises.toString());
     }
 
     @Override
